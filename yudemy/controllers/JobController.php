dc8c09f152433e5cc6d5951ccc102fc8b0ae7fd7
GrytsenkoAndrey <andreyapg@gmail.com>
2020-05-09 18:43:34 +0300

    v 1.33

--- yudemy/controllers/JobController.php
+++ yudemy/controllers/JobController.php
@@ -2,12 +2,29 @@
 
 namespace app\controllers;
 
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use yii\data\Pagination;
+use app\models\Category;
+use app\models\Job;
+
 class JobController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        $name = 'Job :: main';
-        return $this->render('index', ['name' => $name,]);
+        $query = Job::find();
+        $pagination = new Pagination([
+            'defaultPageSize' => 20,
+            'totalCount' => $query->count(),
+        ]);
+        $jobs = $query->orderBy('create_date DESC')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+        return $this->render('index', ['jobs' => $jobs, 'pagination' => $pagination]);
     }
 
     public function actionCreate()
--- yudemy/views/job/index.php
+++ yudemy/views/job/index.php
@@ -1,9 +1,30 @@
 <?php
 /* @var $this yii\web\View */
-?>
-<h1>This page is <?=$name;?></h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+?>
+<h1 class="page-header">Jobs <a href="/index.php?r=job/create" class="btn btn-primary pull-right" title="Create">Create</a></h1>
+
+<?php
+$message = yii::$app->getSession()->getFlash('message');
+if ($message !== null) {
+    ?>
+    <div class="alert alert-success"><?=$message;?></div>
+<?php } ?>
+
+<div class="row">
+
+    <?php foreach ($jobs as $item) { ?>
+    <div class="col-xs-12 col-sm-6 col-md-3 myjob">
+        <h3><?=$item->title;?></h3>
+        <p><strong>Description: </strong><?=$item->description;?></p>
+        <p><strong>City: </strong><?=$item->city;?></p>
+        <p><strong>Address: </strong><?=$item->address;?></p>
+        <p><strong>Listed on: </strong><?=$item->create_date;?></p>
+    </div>
+    <?php } ?>
+
+</div>
+
+<?=LinkPager::widget(['pagination' => $pagination]);?>

febb2be0a3b1fb8c1eb6a99b3a190dc5f864cbc9
Andrey Grytsenko <andreyapg@gmail.com>
2022-08-30 09:50:20 +0300

    Fix Dependabot 2022-08-30

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/package_name',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ff4c8c3dec7c9c430faf72758c248dbcfd3f1ea7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/package_name' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ff4c8c3dec7c9c430faf72758c248dbcfd3f1ea7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
